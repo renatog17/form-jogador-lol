7a181cfceae407408e8ec5d4aeab4302b07a6ed9
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-03-14 23:56:47 -0300

    post jogador funcionando

--- main/postJogador.js
+++ main/postJogador.js
@@ -1,5 +1,5 @@
-async function postJogador(nome, nickmame, divisao, tier, imagem, rolePrimaria, roleSecundaria, campeoes){
-    nome = "renato"
+async function postJogador(nome, nickname, divisao, tier, imagem, rolePrimaria, roleSecundaria, campeoes){
+    divisao = divisao.toUpperCase() 
     const con = await fetch('http://localhost:8080/jogador', {
         method:"POST",
         headers:{
@@ -7,13 +7,29 @@ async function postJogador(nome, nickmame, divisao, tier, imagem, rolePrimaria, 
         },
         body: JSON.stringify({
             nome:nome,
-            nickmame:nickmame,
+            nickname:nickname,
             divisao:divisao,
             tier:tier,
             imagem:imagem,
             rolePrimaria:rolePrimaria,
             roleSecundaria:roleSecundaria,
-            campeoes:campeoes
+            campeoes:["divisao","asdasd"]
         })
     })
 }
+
+var buttonSubmitForm = document.getElementById("bt-enviar-form")
+
+buttonSubmitForm.addEventListener("click", e =>{
+    e.preventDefault()
+    console.log("com sucesso")
+    var nickname = document.getElementById('nickname').value
+    var nome = document.getElementById('nome').value
+    var divisao = document.getElementById('divisao').value
+    var tier = document.getElementById('tier').value
+    var campeoes = document.getElementById('campeoes').value
+    var rolePrimaria = document.getElementById('role-primaria').value
+    var roleSecundaria = document.getElementById('role-secundaria').value
+    var imagem = document.getElementById('imagem').value
+    postJogador(nome, nickname, divisao, tier, imagem, rolePrimaria, roleSecundaria, campeoes)
+})
